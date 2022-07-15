66e6d0f4137b435be12a73d402289f6a735974fa
Lucas Felipe Dalamarta <lfdalamarta@hotmail.com>
2022-07-15 16:25:25 -0300

    Co-authored-by: Victor Dias <victordiasdeveloper@gmail.com> Co-authored-by: Kelvin Morais <kelvinmorais@users.noreply.github.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
+import "antd/dist/antd.css";
 import '../../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Layout } from "antd";
+import { Content } from "antd/lib/layout/layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Content style={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: "100vh"}}>
+        <Component {...pageProps} />
+      </Content>
+    </Layout>
+  )
 }
 
 export default MyApp
